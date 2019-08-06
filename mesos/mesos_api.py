21ddd656f11b9b9f434fe60e0b8642bb96c39b45
zhusicheng <zhusicheng90@gmail.com>
2019-08-06 16:38:58 +0800

    Update mesos_api.py

--- mesos/mesos_api.py
+++ mesos/mesos_api.py
@@ -1,125 +1,44 @@
 import requests
-import json
-
-API_URL = '/api/json'
-QUEUE_API_URL = '/queue' + API_URL
-OVERALL_LOAD_API_URL = '/overallLoad' + API_URL
-COMPUTER_API_URL = '/computer' + API_URL
 
 
-def concatenate_api_url(api_path):
-    if api_path.startswith('/'):
-        return api_path + API_URL
-    else:
-        return '/' + api_path + API_URL
+class MesosAPI:
+    def __init__(self, ip, port=5050, auth_token=''):
+        self.ip = ip
+        self.port = port
+        self.url = 'http://%s:%s' % (ip, port)
+        self.headers = {'authorization': 'Basic %s' % auth_token}
 
-
-class Jenkins:
-    def __init__(self, url, user, passwd):
-        """
-
-        :type url: str
-        :type user: str
-        :type passwd: str
-        """
-        self._url = url
-        self._user = user
-        self._passwd = passwd
-
-    def get(self, url, params=None, timeout=10):
+    def get(self, path, timeout=20.0):
         try:
-            response = requests.get(url, params, auth=(self._user, self._passwd),
+            response = requests.get(self.url + path,
+                                    headers=self.headers,
                                     timeout=timeout)
-            response_json = json.loads(response.text)
-            return response_json
-        except Exception:
-            raise
+            return response
+        except requests.exceptions.RequestException:
+            print 'Get %s failed' % path
+            return None
 
-    def post(self, url, params=None, timeout=10):
-        try:
-            response = requests.post(url, params, auth=(self._user, self._passwd),
-                                     timeout=timeout)
-            print response.status_code
-            response_json = json.loads(response.text)
-            return response_json
-        except Exception:
-            raise
+    def get_leader(self):
+        state = self.get_state()
+        return state.json()['leader']
 
-    def get_info(self):
-        pass
+    def get_state(self, timeout=20.0):
+        return self.get('/state.json', timeout=timeout)
 
-    def get_queue(self, params=None, timeout=10):
-        queue_json = self.get(self._url + QUEUE_API_URL, params, timeout)
-        return queue_json['items']
+    def get_state_summary(self, timeout=20.0):
+        return self.get('/state-summary', timeout=timeout)
 
-    def queueing_jobs_count(self):
-        queue = self.get_queue()
-        return len(queue)
+    def get_version(self):
+        return self.get('/version')
+    
+    def get_metrics_snapshot(self):
+        return self.get('/metrics/snapshot')
 
-    def is_queueing(self):
-        return self.queueing_jobs_count() != 0
+    def get_frameworks(self):
+        return self.get('/frameworks')
 
-    def get_running_builds(self, timeout=10):
-        params = {
-            'tree': 'computer[displayName,executors[currentExecutable[url]],oneOffExecutors[currentExecutable[url]]]',
-            'xpath': '//url',
-            'wrapper': 'builds'
-        }
-        return self.get(self._url + COMPUTER_API_URL, params, timeout)
+    def get_tasks(self):
+        return self.get('/tasks')
 
-    def get_running_builds_list(self):
-        running_builds = self.get_running_builds()
-        running_builds_list = []
 
-        for computer in running_builds['computer']:
 
-            for executor in computer['executors']:
-                if executor['currentExecutable'] is not None:
-                    running_build_url = executor['currentExecutable']['url']
-                    running_builds_list.append(running_build_url)
-
-            for executor in computer['oneOffExecutors']:
-                if executor['currentExecutable'] is not None:
-                    running_build_url = executor['currentExecutable']['url']
-                    running_builds_list.append(running_build_url)
-
-        return running_builds_list
-
-    def is_job_running(self):
-        return len(self.get_running_builds_list()) != 0
-
-    def get_jobs(self, params=None, timeout=10):
-        api_json = self.get(self._url + API_URL, params, timeout)
-        jobs = api_json['jobs']
-        return jobs
-
-    def get_jobs_name_list(self):
-        jobs_name_list = []
-        jobs = self.get_jobs()
-        for job in jobs:
-            jobs_name_list.append(job['name'])
-        return jobs_name_list
-
-    def get_views(self, params=None, timeout=10):
-        api_json = self.get(self._url + API_URL, params, timeout)
-        views = api_json['views']
-        return views
-
-    def get_computers(self, params=None, timeout=10):
-        computer_json = self.get(self._url + COMPUTER_API_URL, params, timeout)
-        return computer_json
-
-    def get_job_conf_xml(self, name):
-        pass
-
-    def restart(self):
-        pass
-
-    def safe_restart(self):
-        pass
-
-    def shutdown(self):
-        pass
-
-    def safe_shutdown(self):
-        pass
